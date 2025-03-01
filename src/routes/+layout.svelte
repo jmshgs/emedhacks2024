861a69a88efa358d82f25e42bcc01f02d89e059d
jxhug <james@jxhug.me>
2025-03-01 14:42:39 -0800

    working :D

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,14 +5,17 @@ import { ModeWatcher } from "mode-watcher";
 
 import ThemeSwitcher from "$lib/components/themeSwitcher.svelte";
 
+import Sidebar from "$lib/components/sidebar.svelte";
+
 </script>
 
 <ModeWatcher/>
 
 
-<div class="h-[6em]">
+<div class="flex flex-row">
+    <Sidebar />
+    <div class="animate-appear animate-in fade-in zoom-in w-screen h-screen flex flex-col pt-[6em] overflow-scroll">
+        <slot/>
+    </div>
 </div>
 
-<div class="animate-appear animate-in fade-in zoom-in w-screen h-screen flex flex-col">
-    <slot/>
-</div>
